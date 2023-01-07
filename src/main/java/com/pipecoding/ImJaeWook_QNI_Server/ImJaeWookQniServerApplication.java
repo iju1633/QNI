fe28babbe8cb109a706d0235a9d6e85581915626
hongdroid94 <hongdroid94@gmail.com>
2023-01-07 16:54:51 +0900

    first commit

---
+++ src/main/java/com/pipecoding/ImJaeWook_QNI_Server/ImJaeWookQniServerApplication.java
@@ -0,0 +1,13 @@
+package com.pipecoding.ImJaeWook_QNI_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImJaeWookQniServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImJaeWookQniServerApplication.class, args);
+	}
+
+}
