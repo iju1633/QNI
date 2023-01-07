79daabc9bdc6ad20f6cca8c17ee55d5f10a5a482
Garfield <iju1633@gmail.com>
2023-01-07 21:54:36 +0900

    - application.yml에서 url을 관리할 수 있도록 어노테이션 설정

--- src/main/java/com/pipecoding/ImJaeWook_QNI_Server/ImJaeWookQniServerApplication.java
+++ src/main/java/com/pipecoding/ImJaeWook_QNI_Server/ImJaeWookQniServerApplication.java
@@ -1,9 +1,12 @@
 package com.pipecoding.ImJaeWook_QNI_Server;
 
+import com.pipecoding.ImJaeWook_QNI_Server.util.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ApplicationProperties.class)
 public class ImJaeWookQniServerApplication {
 
 	public static void main(String[] args) {
