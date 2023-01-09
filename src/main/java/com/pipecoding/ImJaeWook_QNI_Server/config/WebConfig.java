ddbafadfeb3c9c45551bef7ba808370158dfb9cb
Garfield <iju1633@gmail.com>
2023-01-09 14:32:41 +0900

    - 로그인 여부를 확인하는 인터셉터 구현

---
+++ src/main/java/com/pipecoding/ImJaeWook_QNI_Server/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.pipecoding.ImJaeWook_QNI_Server.config;
+
+import com.pipecoding.ImJaeWook_QNI_Server.util.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor()) // 인터셉터 등록
+                .order(1) // 낮을 수록 먼저 호출
+                .addPathPatterns("/answer/**", "/question/**", "/word/**"); //인터셉터를 적용할 url 패턴
+    }
+}
